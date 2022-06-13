8c52e4f2e02c76730c6cd6dc22d5552bb8bc43ec
kranthigy <kranthi@goodyear.com>
2022-06-13 10:04:56 -0400

    added new page to the website.

--- js/script.js
+++ js/script.js
@@ -122,6 +122,10 @@ $(function() {
    });
 });
 
+
+
+
+
 /** Stats Section **/
 
 $(function() {
@@ -131,4 +135,20 @@ $(function() {
 
     });
 
+});
+
+/** Clients **/
+$(function() {
+    $('#clients-list').owlCarousel({
+        items:7,
+        autoplay:true,
+        smartSpeed:700,
+        loop:true,
+        autoplayHoverPause : true,
+        nav: true,
+        dots: false,
+        navText:['<i class="fa fa-solid fa-angle-left"></i>','<i class="fa fa-angle-right" ></i>']
+
+
+    });
 });
